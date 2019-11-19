b0ddea75e1fdcd20cf45bd1057799e834e476183
chance <zhiquan911@qq.com>
2019-11-19 16:04:03 +0800

    SupportBlockchainDAI

--- openwtester/subscribe_test.go
+++ openwtester/subscribe_test.go
@@ -17,6 +17,7 @@ package openwtester
 
 import (
 	"github.com/astaxie/beego/config"
+	"github.com/blocktree/openwallet/common/file"
 	"github.com/blocktree/openwallet/log"
 	"github.com/blocktree/openwallet/openw"
 	"github.com/blocktree/openwallet/openwallet"
@@ -95,6 +96,18 @@ func TestSubscribeAddress_TRUE(t *testing.T) {
 
 	//log.Debug("already got scanner:", assetsMgr)
 	scanner := assetsMgr.GetBlockScanner()
+	if scanner.SupportBlockchainDAI() {
+		dbFilePath := filepath.Join("data", "db")
+		dbFileName := "blockchain.db"
+		file.MkdirAll(dbFilePath)
+		dai, err := openwallet.NewBlockchainLocal(filepath.Join(dbFilePath, dbFileName), false)
+		if err != nil {
+			log.Error("NewBlockchainLocal err: %v", err)
+			return
+		}
+
+		scanner.SetBlockchainDAI(dai)
+	}
 	//scanner.SetRescanBlockHeight(6518561)
 
 	if scanner == nil {
